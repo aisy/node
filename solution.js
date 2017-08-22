ef44c553436b6c919d01594d6a777bb8ad9e99f0
aisy <aisymuhammad@gmail.com>
2017-08-22 09:58:38 +0700

    add json

--- solution.js
+++ solution.js
@@ -1,25 +1,25 @@
 
 // load module
-var express = require('express');
-var bodyParser = require('body-parser');
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var http        = require('http');
+var fs          = require('fs');
 
 //  initial apps
 var app = express();
+
 
 // use bodyparser to read json
 app.use(bodyParser.json());
 
 // initial link
 app.get('/',function(req,res){
-    res.send('get request');
-});
-
-// initial link
-app.get('/object', function(req,res){
-    var obj={nama:'AisyMr',karir:'Software Engineer'};
+    // var obj={nama:'AisyMr',karir:'Software Engineer'};
+    var obj = JSON.parse(fs.readFileSync('solution.json','utf8'));
 	res.send(obj);
 });
 
-app.listen(3000, function(){
-    console.log('listening on port 3000');
+// set port
+app.listen(8000, function(){
+    console.log('listening on port 8000');
 });
