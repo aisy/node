708b5f3f7f8160cc680897bdefab970141175cd6
aisy <aisymuhammad@gmail.com>
2017-08-22 15:02:21 +0700

    fix link1

--- solution.js
+++ solution.js
@@ -7,41 +7,42 @@ var link2 = 'https://m.bnizona.com/promo/index/16';
 var title=[];
 var json=[];
 
-// osmosis(link1)
-//     .find('.menu li')
-//     .set({
-//         Category: 'a',
-//     })
+osmosis(link1)
+    .find('.menu li')
+    .set({
+        "category" : 'a',
+        "link": 'a@href'
+    })
+    .data(function(data){
+        console.log(data)
+        title.push(data)
+    })
+    // .log(console.log)
+    // .debug(console.log)
+    // .error(console.log)
+    .done(function(){
+        var file= 'solution.json';
+        jsonfile.writeFile(file, title);
+    });
+
+// osmosis(link2)
+//     .find('.list2 li')
+//     .set(
+//         {
+//          "merchan-name"   : ".merchant-name",
+//          "imageurl"       : "img@src",
+//          "promo-title"    : ".promo-title",
+//          "valid-until"    : ".valid-until"   
+//         })
 //     .data(function(data){
-//         console.log(data)
-//         title.push(data)
-//     })
-//     // .log(console.log)
-//     // .debug(console.log)
-//     // .error(console.log)
+//             console.log(data);
+//             json.push(data);
+//         }
+//     )
+//     .log(console.log)
+//     .debug(console.log)
+//     .error(console.log)
 //     .done(function(){
 //         var file= 'solution.json';
 //         jsonfile.writeFile(file, json);
 //     });
-
-osmosis(link2)
-    .find('.list2 li')
-    .set(
-        {
-         "merchan-name"   : ".merchant-name",
-         "imageurl"       : "img@src",
-         "promo-title"    : ".promo-title",
-         "valid-until"    : ".valid-until"   
-        })
-    .data(function(data){
-            console.log(data);
-            json.push(data);
-        }
-    )
-    .log(console.log)
-    .debug(console.log)
-    .error(console.log)
-    .done(function(){
-        var file= 'solution.json';
-        jsonfile.writeFile(file, json);
-    });
